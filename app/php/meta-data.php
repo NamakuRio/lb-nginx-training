5d27104d3caaea556c105da52c2024380b7eb546
Rio Prastiawan <akunviprio@gmail.com>
2021-07-06 16:04:49 +0700

    update nginx conf and meta data in php app

--- app/php/meta-data.php
+++ app/php/meta-data.php
@@ -1,5 +1,13 @@
 <?php
+$timestamp = (new DateTime())->format('F dS Y, h:i:s a');
 
-require('response.php');
+$data = [
+    "description" => "Just an example metadata.",
+    "external_url" => "https://avatars.githubusercontent.com/u/34277951?&u=e35a3b2b6bc24f66e760400f44768e9e2223578e",
+    "image" => "https://avatars.githubusercontent.com/u/34277951?&u=e35a3b2b6bc24f66e760400f44768e9e2223578e",
+    "name" => "Profile Picture",
+    "timestamp" => $timestamp
+];
+
 header('Content-Type: application/json');
 echo json_encode($data);
